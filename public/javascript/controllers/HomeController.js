682b2ab13378b42ba40873e471514e5824e63d92
M-Doja <micdoja@yahoo.com>
2015-10-24 22:09:07 -0700

    States display

--- public/javascript/controllers/HomeController.js
+++ public/javascript/controllers/HomeController.js
@@ -3,67 +3,64 @@
 	angular.module('app')
 	.controller('HomeController', HomeController);
 
-	HomeController.$inject = [];
-
 	function HomeController() {
 		var vm = this;
 		vm.title = 'Welcome to TraveLaws!';
 		vm.head = 'An app for travellers to know the laws of the land.';
 
-		vm.States = [{
-			ALABAMA: {gen: '', traf: '', gun: '', bus: ''},
-			ALASKA: {gen: '', traf: '', gun: '', bus: ''},
-			ARIZONA: {gen: '', traf: '', gun: '', bus: ''},
-			ARKANSAS: {gen: '', traf: '', gun: '', bus: ''},
-			CALIFORNIA: {gen: '', traf: '', gun: '', bus: ''},
-			COLORADO: {gen: '', traf: '', gun: '', bus: ''},
-			CONNECTICUT: {gen: '', traf: '', gun: '', bus: ''},
-  		DELAWARE: {gen: '', traf: '', gun: '', bus: ''},
-			FLORIDA: {gen: '', traf: '', gun: '', bus: ''},
-			GEORGIA: {gen: '', traf: '', gun: '', bus: ''},
-			HAWAII: {gen: '', traf: '', gun: '', bus: ''},
-			IDAHO: {gen: '', traf: '', gun: '', bus: ''},
-			ILLINOIS: {gen: '', traf: '', gun: '', bus: ''},
-			INDIANA: {gen: '', traf: '', gun: '', bus: ''},
-			IOWA: {gen: '', traf: '', gun: '', bus: ''},
-  		KANSAS: {gen: '', traf: '', gun: '', bus: ''},
-			KENTUCKY: {gen: '', traf: '', gun: '', bus: ''},
-			LOUISIANA: {gen: '', traf: '', gun: '', bus: ''},
-			MAINE: {gen: '', traf: '', gun: '', bus: ''},
-			MARYLAND: {gen: '', traf: '', gun: '', bus: ''},
-			MASSACHUSETTS: {gen: '', traf: '', gun: '', bus: ''},
-			MICHIGAN: {gen: '', traf: '', gun: '', bus: ''},
-  		MINNESOTA: {gen: '', traf: '', gun: '', bus: ''},
-			MISSISSIPPI: {gen: '', traf: '', gun: '', bus: ''},
-			MISSOURI: {gen: '', traf: '', gun: '', bus: ''},
-			MONTANA: {gen: '', traf: '', gun: '', bus: ''},
-			NEBRASKA: {gen: '', traf: '', gun: '', bus: ''},
-			NEVADA: {gen: '', traf: '', gun: '', bus: ''},
-			NEW_HAMPSHIRE: {gen: '', traf: '', gun: '', bus: ''},
-   		NEW_JERSEY: {gen: '', traf: '', gun: '', bus: ''},
-			NEW_MEXICO: {gen: '', traf: '', gun: '', bus: ''},
-			NEW_YORK: {gen: '', traf: '', gun: '', bus: ''},
-			NORTH_CAROLINA: {gen: '', traf: '', gun: '', bus: ''},
-			NORTH_DAKOTA: {gen: '', traf: '', gun: '', bus: ''},
-			OHIO: {gen: '', traf: '', gun: '', bus: ''},
-			OKLAHOMA: {gen: '', traf: '', gun: '', bus: ''},
-    	OREGON: {gen: '', traf: '', gun: '', bus: ''},
-			PENNSYLVANIA: {gen: '', traf: '', gun: '', bus: ''},
-			RHODE_ISLAND: {gen: '', traf: '', gun: '', bus: ''},
-			SOUTH_CAROLINA: {gen: '', traf: '', gun: '', bus: ''},
-			SOUTH_DAKOTA: {gen: '', traf: '', gun: '', bus: ''},
-			TENNESSEE: {gen: '', traf: '', gun: '', bus: ''},
-			TEXAS: {gen: '', traf: '', gun: '', bus: ''},
-			UTAH: {gen: '', traf: '', gun: '', bus: ''},
-      VERMONT: {gen: '', traf: '', gun: '', bus: ''},
-			VIRGINIA: {gen: '', traf: '', gun: '', bus: ''},
-			WASHINGTON: {gen: '', traf: '', gun: '', bus: ''},
-			WEST_VIRGINIA: {gen: '', traf: '', gun: '', bus: ''},
-			WISCONSIN: {gen: '', traf: '', gun: '', bus: ''},
-			WYOMING: {gen: '', traf: '', gun: '', bus: ''}
-}];
-
-
-
+		vm.States = [
+			{state: 'ALABAMA', laws: {gen : 'WWW.ca.gov',traf : 'WWWWWW',gun : 'WWWWW',bus : 'WW'}},
+			{state: 'ALASKA', laws: {gen : 'WWW.ca.gov',traf : 'WWWWWW',gun : 'WWWWW',bus : 'WW'}},
+			{state: 'ARIZONA', laws: {gen : 'WWW.ca.gov',traf : 'WWWWWW',gun : 'WWWWW',bus : 'WW'}},
+			{state: 'ARKANSAS', laws: {gen : 'WWW.ca.gov',traf : 'WWWWWW',gun : 'WWWWW',bus : 'WW'}},
+			{state: 'CALIFORNIA', laws: {gen : 'WWW.ca.gov',traf : 'WWWWWW',gun : 'WWWWW',bus : 'WW'}}
+];
+			// state:  console.log(vm.AL.traf);
 	}
 })();
+			//
+			// state: COLORADO, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: CONNECTICUT, laws: {gen: '', traf: '', gun: '', bus: ''},
+  		// state: DELAWARE, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: FLORIDA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: GEORGIA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: HAWAII, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: IDAHO, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: ILLINOIS, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: INDIANA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: IOWA, laws: {gen: '', traf: '', gun: '', bus: ''},
+  		// state: KANSAS, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: KENTUCKY, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: LOUISIANA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: MAINE, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: MARYLAND, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: MASSACHUSETTS, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: MICHIGAN, laws: {gen: '', traf: '', gun: '', bus: ''},
+  		// state: MINNESOTA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: MISSISSIPPI, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: MISSOURI, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: MONTANA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: NEBRASKA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: NEVADA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: NEW_HAMPSHIRE, laws: {gen: '', traf: '', gun: '', bus: ''},
+   	// state: 	NEW_JERSEY, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: NEW_MEXICO, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: NEW_YORK, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: NORTH_CAROLINA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: NORTH_DAKOTA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: OHIO, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: OKLAHOMA, laws: {gen: '', traf: '', gun: '', bus: ''},
+    	// state: OREGON, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: PENNSYLVANIA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: RHODE_ISLAND, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: SOUTH_CAROLINA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: SOUTH_DAKOTA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: TENNESSEE, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: TEXAS, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: UTAH, laws: {gen: '', traf: '', gun: '', bus: ''},
+      // state: VERMONT, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: VIRGINIA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: WASHINGTON, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: WEST_VIRGINIA, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: WISCONSIN, laws: {gen: '', traf: '', gun: '', bus: ''},
+			// state: WYOMING, laws: {gen: '', traf: '', gun: '', bus: ''}
